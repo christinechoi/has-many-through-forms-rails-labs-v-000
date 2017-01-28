dd13a4ec32195d9d60c8c70d262bd0d556965d16
christinechoi <smi77y@gmail.com>
2017-01-27 21:36:09 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,8 +12,6 @@ class PostsController < ApplicationController
   end
 
   def create
-
-
     post = Post.create(post_params)
     redirect_to post
   end
@@ -21,7 +19,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+    params.require(:post).permit(:title, :content, category_ids: [], categories_attributes: [:name])
   end
 
 
